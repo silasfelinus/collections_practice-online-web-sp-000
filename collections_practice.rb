e212b9c7c1edfc46ab6cbcf03780d4f83874d9d0
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-19 06:15:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,62 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  element1 = array[1]
+  element2 = array[2]
+  new_array = []
+  array.each_with_index do |element, index|
+    if index == 1
+      new_array << element2
+    elsif index == 2
+      new_array << element1
+    else
+      new_array << element
+    end
+  end
+  new_array
+end
+
+def reverse_array(array)
+  array.reverse
+
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    new_array << word[0..1] + "$" + word[3,word.length-1]
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select {|string| string[0] == "a"}
+end
+
+def sum_array(array)
+  sum = 0
+  sum += array.each
+  sum
+end
+
+def add_s(array)
+
+
+end
